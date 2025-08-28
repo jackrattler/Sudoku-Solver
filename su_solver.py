b136d4693de1dbd723c466c4e63b9365db53fd84
Jack Stonell <jackstonell.uk@gmail.com>
2025-08-28 12:50:52 +0100

    refactored a little and updated 'find_uniques'function to work properly(?)

--- su_solver.py
+++ su_solver.py
@@ -8,6 +8,9 @@ tst_board = [[4,0,3,0,0,0,1,0,5],
              [0,0,0,0,4,0,5,0,0],
              [6,0,9,1,0,0,0,8,0]]
 
+valids_board = [[0 for i in range(9)] for j in range(9)]                              #initialise board to hold valid digits
+
+
 def display_board(board):
     for i in range (9):                     #loop through row
         if i == 3 or i == 6:
@@ -50,8 +53,46 @@ def find_valid(board, pos):
 
     return digits
 
-def solve(board):
-    valids_board = [[0 for i in range(9)] for j in range(9)]                              #initialise board to hold valid digits
+def find_uniques():
+    if not find_empty(tst_board): return True        #no empties means the puzzle has been solved
+
+    for i in range(9):
+        for j in range(9):
+            if not valids_board[i][j] == 0:                      #loop through valids_board searching for unsolved cells
+                pos = i,j                                   
+                exclusive = set(valids_board[pos[0]][pos[1]])
+                for x in range(9):
+                    if valids_board[i][x] and not (i,x) == (pos):        #if not the current cell and not a solved cell
+                        exclusive = exclusive - set(valids_board[i][x])  #subtract the valids from our current cells valids  
+                for y in range(9):
+                    if valids_board[y][j] and not (y,j) == (pos):
+                        exclusive = exclusive - set(valids_board[y][j])
+
+                for x in range (int(i/3), int(i/3) * 3 + 3):            #3x3 grid check
+                    for y in range(int(j/3), int(j/3)* 3 + 3 ):
+                        if valids_board[x][y] and not (x,y) == (pos):
+                            exclusive = exclusive - set(valids_board[x][y])
+                    
+                #if a digit is only valid for this cell within its row, column or 3x3 grid
+                #pop it in the board and remove the valids 
+                if len(exclusive) == 1:                                 
+                    tst_board[pos[0]][pos[1]] = list(exclusive)[0]
+                    valids_board[pos[0]][pos[1]] = 0
+                    return find_uniques()               #if a cells solution has been found start again until no cell solutions are found
+
+                    
+
+
+def brute(v_board):
+    pass
+                    
+                            
+
+def is_valid(board):
+    pass
+
+def simple_solve(board):
+    
 
     while find_empty(tst_board):
         empty_pos = find_empty(tst_board)                   #find all empty cells 
@@ -61,52 +102,39 @@ def solve(board):
 
             if len(valids) == 1:                            #if only 1 valid number
                 tst_board[i[0]][i[1]] = valids[0]           #insert that number to the board
-                valids_board[i[0]][i[1]] = 0                #remove any digits from the corresponding valids_board
+                valids_board[i[0]][i[1]] = 0                #remove any digits from the corresponding valids_board cell
                 continue
 
+            if len(valids) > 1 and i == empty_pos[-1]:      #if we exhaust the empties and still can't solve
+                return False                                #get outta here
 
-        find_uniques(valids_board)
-
-
-
-
-            #if i == empty_pos[-1]:                         #if we reach the final empty cell and still don't have a solution...
-             #   break                                      #break out 
-
+        #####find rows, columns or 3x3s where a digit is only valid for 1 element        
+        #valids_board = find_uniques(valids_board)           
         
-        break
+        
+    return True                                             #no more empties- puzzle solved
+        
+        
+        
 
-def find_uniques(v_board):
-    for i in range(9):
-        for j in range(9):
-            if not v_board[i][j] == 0:                      #loop through valids_board searching for unsolved cells
-                pos = i,j                                   
-                exclusive = set(v_board[pos[0]][pos[1]])
-                for x in range(9):
-                    if v_board[i][x] and not (i,x) == (pos):        #if not the current cell and not a solved cell
-                        exclusive = exclusive - set(v_board[i][x])  #subtract the valids from our current cells valids  
-                for y in range(9):
-                    if v_board[y][j] and not (y,j) == (pos):
-                        exclusive = exclusive - set(v_board[y][j])
 
-                print("\n\n")##############################
-                display_board(tst_board)#####################
-
-                for x in range (int(i/3), int(i/3) * 3 + 3):            #3x3 grid check
-                    for y in range(int(j/3), int(j/3)* 3 + 3 ):
-                        if v_board[x][y] and not (x,y) == (pos):
-                            exclusive = exclusive - set(v_board[x][y])
-                    
-
-                if len(exclusive) == 1:
-                    tst_board[pos[0]][pos[1]] = list(exclusive)[0]
 
 
 #########start of program###########
 
 display_board(tst_board)
 #input("Press Enter to solve")  
-solve(tst_board)
+if simple_solve(tst_board):
+    display_board(tst_board)
+    print("\n\nSolved using the simple solve method")
 
-print("\n\n")
-display_board(tst_board)
+if find_uniques():
+    display_board(tst_board)
+    print("\n\nSolved using 'find uniques' method")
+
+
+
+else:
+
+    print("\n\nI cannae do it captain. Here's how far I got")
+    display_board(tst_board)
