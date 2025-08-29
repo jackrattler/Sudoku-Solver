93428dfc01575bd993fc67bdfbeb5215ef3fe0a5
Jack Stonell <jackstonell.uk@gmail.com>
2025-08-29 12:18:35 +0100

    began creating GUI for solver in Godot

--- sudoku-solver/grid_container.gd
+++ sudoku-solver/grid_container.gd
@@ -1,8 +1,7 @@
 extends GridContainer
 
-var GRID_SIZE = 9
-@export var CELL_SIZE = size.x / GRID_SIZE
-
+@onready var GRID_SIZE = 9
+@onready var th = load("res://main.tres") as Theme
 
 func _ready() -> void:
 	var board = []				#set up blank board
@@ -14,10 +13,21 @@ func _ready() -> void:
 		
 	for y in GRID_SIZE:	
 		for x in GRID_SIZE:
-			var cell = Button.new()
-			cell.position = Vector2(y*CELL_SIZE, x*CELL_SIZE)
-			cell. = Color(255,255,255,255)
+			var cell = Label.new()
+			add_child(cell)
+			cell.text = '0'
+			cell.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+			cell.size_flags_vertical = Control.SIZE_EXPAND_FILL
+			cell.vertical_alignment = 1
+			cell.horizontal_alignment = 1
+			cell.mouse_filter = Control.MOUSE_FILTER_STOP
 			
-		
+func _process(delta: float) -> void:
+	pass		
 		
 	
+
+
+func _on_mouse_entered() -> void:
+	print("mouse entered")
+	th.clear_color("font_color", "Label")
